392ceed7a726b0a4d01b0af1b95b4ec76f674111
Luan Rodrigues <luanrodriguesbusiness@hotmail.com>
2019-06-23 17:28:14 -0300

    insert aluno_turmas

---
+++ inserts/aluno_turma.sql
@@ -0,0 +1,14 @@
+﻿insert into aluno_turma(id_aluno_turma, id_aluno, id_turma, data) values
+-- 2019
+(1, 1, 3, now()),
+(2, 3, 3, now()),
+(3, 5, 3, now()),  -- repetiu de ano
+(4, 2, 4, now()),
+(5, 4, 4, now()),
+-- 2018
+(6, 1, 1, '2018-2-10'),
+(7, 3, 1, '2018-2-10'),  -- repetiu de ano
+(8, 5, 3, '2018-2-10'),
+--2017
+(9, 3, 1, '2017-2-10'),
+(10, 5, 2, '2017-2-10');
