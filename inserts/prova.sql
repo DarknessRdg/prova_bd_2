42e70f5bf45b4a9d5f381a056fafbadba1719f73
Luan Rodrigues <luanrodriguesbusiness@hotmail.com>
2019-06-24 09:34:11 -0300

    insert prova

---
+++ inserts/prova.sql
@@ -0,0 +1,10 @@
+insert into prova(id_prova, id_disciplina, finalizada) values 
+(1, 1, false), 
+(2, 3, false), 
+(3, 6, false), 
+(4, 8, false), 
+(5, 10, false), 
+(6, 12, false),
+(7, 13, false),
+(8, 15, false),
+(9, 17, false);
