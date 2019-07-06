c8f025a2234658bb69d155bf1f909b4df814d722
Paulo Vilarinho <paulotarsobranco@hotmail.com>
2019-07-05 23:11:50 -0300

    tinha esquecido de salvar o trigger kkkkk

--- triggers/resposta.sql
+++ triggers/resposta.sql
@@ -3,34 +3,37 @@
 --     * SOMENTE 1 RESPOSTA POR ALUNO POR QUESTAO POR PROVA
 --     * ALUNO MATRICULADO NA MESMA TURMA DA APLICACAO
 
- 
+
 CREATE OR REPLACE FUNCTION validate_resposta() RETURNS TRIGGER AS $$
 DECLARE
-    prova record;
-    aula record;
+    gabarito_new record;
+	aplicacao_new record;
+	aula_new record;
     diferenca INT;
 BEGIN
-    SELECT * INTO aula FROM aula WHERE id_aula = NEW.id_aula;
-    SELECT * INTO prova FROM prova WHERE id_prova = NEW.id_prova;
-    SELECT EXTRACT(YEAR FROM NEW.data_aplicacao) - EXTRACT(YEAR FROM MAX(data_aplicacao)) dif INTO diferenca
-        FROM aplicacao WHERE id_aula = NEW.id_aula AND id_prova = NEW.id_prova;
- 
-    IF  aula.id_disciplina != prova.id_disciplina THEN
-        raise exception 'A disciplina da prova deve ser igual a disciplina da aula.';
-    ELSIF diferenca <  3 THEN
-        raise exception 'A prova so pode ser repetida para a mesma turma a cada 3 anos.';
-    ELSIF EXTRACT(YEAR FROM NEW.data_aplicacao) < aula.ano_letivo THEN
-        raise exception 'Nao é possivel cadastrar aplicacoes para aulas de anos anteriores.';
-    ELSIF EXTRACT(YEAR FROM NEW.data_aplicacao) > aula.ano_letivo THEN 
-        raise exception 'Nao é possivel cadastrar aplicacoes para aulas de anos futuros.';
-	ELSIF prova.finalizada = false THEN
-		raise exception 'Não é possivel cadastrar aplicações com provas não finalizadas';
-    END IF;
+    SELECT * INTO gabarito_new FROM gabarito WHERE id_gabarito = NEW.id_gabarito;
+	SELECT * INTO aplicacao_new FROM aplicacao WHERE id_aplicacao = NEW.id_aplicacao;
+	SELECT * INTO aula_new from aula where id_aula = aplicacao_new.id_aula;
+	
+    if gabarito_new.id_questao in (Select distinct id_questao FROM gabarito 
+	WHERE id_gabarito in (select id_gabarito from resposta where id_aluno  = new.id_aluno)and id_prova = gabarito_new.id_prova) then
+   		raise exception 'essa questão já foi respondida pelo aluno ';
+	end if;
+	
+	if new.id_aluno not in (select id_aluno from aluno_turma where id_turma = aula_new.id_turma and extract(year from aluno_turma.data) = aula_new.ano_letivo) then
+		raise exception 'esse aluno não esta matriculado na turma da aplicaçõa dessa prova';
+   	end if;
    
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+
+
  
  
 CREATE TRIGGER verificar_resposta BEFORE INSERT OR UPDATE ON resposta
     FOR EACH ROW EXECUTE PROCEDURE validate_resposta();
+	
+
+
