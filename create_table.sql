eda88eefb6befb3e35ffc4a7af41ab6f4246563d
Experluan <luanrodrigues007@hotmail.com>
2019-07-10 08:22:33 -0300

    on delete - create

--- create_table.sql
+++ create_table.sql
@@ -22,13 +22,13 @@ CREATE TABLE turma(
     sigla char(1) NOT NULL,
     turno char(5), NOT NULL,
     e_medio bool NOT NULL,
-    id_escola INT NOT NULL REFERENCES escola(id_escola)
+    id_escola INT NOT NULL REFERENCES escola(id_escola) ON DELETE CASCADE
 );
  
 CREATE TABLE aluno_turma(
     id_aluno_turma serial NOT NULL PRIMARY KEY,
-    id_aluno INT NOT NULL REFERENCES aluno(id_aluno),
-    id_turma INT NOT NULL REFERENCES turma(id_turma),
+    id_aluno INT NOT NULL REFERENCES aluno(id_aluno) ON DELETE CASCADE,
+    id_turma INT NOT NULL REFERENCES turma(id_turma) ON DELETE CASCADE,
     DATA DATE DEFAULT NOW()
 );
  
@@ -49,30 +49,30 @@ CREATE TABLE disciplina(
  
 CREATE TABLE aula(
     id_aula serial NOT NULL PRIMARY KEY,
-    id_turma INT NOT NULL REFERENCES turma(id_turma),
-    id_professor INT NOT NULL REFERENCES professor(id_professor),
-    id_disciplina INT NOT NULL REFERENCES disciplina(id_disciplina),
+    id_turma INT NOT NULL REFERENCES turma(id_turma) ON DELETE CASCADE,
+    id_professor INT NOT NULL REFERENCES professor(id_professor) ON DELETE CASCADE,
+    id_disciplina INT NOT NULL REFERENCES disciplina(id_disciplina) ON DELETE CASCADE,
     ano_letivo INT DEFAULT EXTRACT(YEAR FROM NOW())
 );
  
 CREATE TABLE prova(
     id_prova serial NOT NULL PRIMARY KEY,
-    id_disciplina INT REFERENCES disciplina(id_disciplina),
+    id_disciplina INT REFERENCES disciplina(id_disciplina) ON DELETE CASCADE,
     finalizada bool DEFAULT FALSE
 );
  
  
 CREATE TABLE aplicacao(
     id_aplicacao serial NOT NULL PRIMARY KEY,
-    id_aula INT NOT NULL REFERENCES aula(id_aula),
-    id_prova INT NOT NULL REFERENCES prova(id_prova),
+    id_aula INT NOT NULL REFERENCES aula(id_aula) ON DELETE CASCADE,
+    id_prova INT NOT NULL REFERENCES prova(id_prova) ON DELETE CASCADE,
     data_aplicacao DATE NOT NULL
 );
  
 CREATE TABLE questao(
     id_questao serial NOT NULL PRIMARY KEY,
     enunciado varchar(500) NOT NULL,
-    id_disciplina INT NOT NULL REFERENCES disciplina(id_disciplina),
+    id_disciplina INT NOT NULL REFERENCES disciplina(id_disciplina) ON DELETE CASCADE,
     finalizada bool DEFAULT FALSE
 );
  
@@ -83,14 +83,14 @@ CREATE TABLE alternativa(
  
 CREATE TABLE gabarito (
     id_gabarito serial NOT NULL PRIMARY KEY,
-    id_prova INT NOT NULL REFERENCES prova(id_prova),
-    id_questao INT NOT NULL REFERENCES questao(id_questao),
-    id_alternativa INT NOT NULL REFERENCES alternativa(id_alternativa),
+    id_prova INT NOT NULL REFERENCES prova(id_prova) ON DELETE CASCADE,
+    id_questao INT NOT NULL REFERENCES questao(id_questao) ON DELETE CASCADE,
+    id_alternativa INT NOT NULL REFERENCES alternativa(id_alternativa) ON DELETE CASCADE,
     e_certo bool NOT NULL
 );
  
 CREATE TABLE resposta(
     id_resposta serial NOT NULL PRIMARY KEY,
-    id_aluno INT NOT NULL REFERENCES aluno(id_aluno),
-    id_gabarito INT NOT NULL REFERENCES gabarito(id_gabarito)
+    id_aluno INT NOT NULL REFERENCES aluno(id_aluno) ON DELETE CASCADE,
+    id_gabarito INT NOT NULL REFERENCES gabarito(id_gabarito) ON DELETE CASCADE
 );
